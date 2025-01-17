b1c32cad4be906bf8520d885500371f5d0ecef7c
Tobias O <tobias.olenyi@tum.de>
2025-01-17 12:03:39 +0100

    refactor: add core exports

--- src/flatprot/core/__init__.py
+++ src/flatprot/core/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .components import Chain, Residue, Structure
-from .secondary import SecondaryStructure, SecondaryStructureType
+from .secondary import SecondaryStructure, SecondaryStructureType, Helix, Sheet, Coil
 from .manager import CoordinateManager
 
 all = [
@@ -11,5 +11,8 @@ all = [
     Structure,
     SecondaryStructure,
     SecondaryStructureType,
+    Helix,
+    Sheet,
+    Coil,
     CoordinateManager,
 ]
