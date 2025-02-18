f4525676f9491c61f2aa12cd64721d4464229c72
Tobias O <tobias.olenyi@tum.de>
2025-02-18 15:59:10 +0100

    style: add non-element drawing

--- src/flatprot/drawing/structure.py
+++ src/flatprot/drawing/structure.py
@@ -3,7 +3,12 @@
 from drawsvg import DrawingElement, Path, Line
 import numpy as np
 
-from ..scene.structure import CoilElement, HelixElement, SheetElement
+from ..scene.structure import (
+    CoilElement,
+    HelixElement,
+    SheetElement,
+    StructureSceneElement,
+)
 
 
 def draw_coil(element: CoilElement) -> DrawingElement:
@@ -29,19 +34,23 @@ def draw_coil(element: CoilElement) -> DrawingElement:
     return path
 
 
+def draw_non_element(element: StructureSceneElement) -> DrawingElement:
+    return Line(
+        *element.coordinates[0],
+        *element.coordinates[-1],
+        stroke=element.style.fill_color,
+        stroke_width=element.style.line_width * element.style.stroke_width_factor,
+        class_="helix",
+    )
+
+
 def draw_helix(element: HelixElement) -> DrawingElement:
     """Draw a helix element"""
 
     coords = element.display_coordinates
 
     if len(element.coordinates) <= element.style.min_helix_length:
-        return Line(
-            *element.coordinates[0],
-            *element.coordinates[-1],
-            stroke=element.style.stroke_color,
-            stroke_width=element.style.line_width * element.style.stroke_width_factor,
-            class_="helix",
-        )
+        return draw_non_element(element)
 
     path = Path(
         stroke=element.style.stroke_color,
@@ -74,15 +83,7 @@ def draw_sheet(element: SheetElement) -> DrawingElement:
         ).all()
         or len(element.coordinates) <= element.style.min_sheet_length
     ):
-        return Line(
-            coords[0][0],
-            coords[0][1],
-            coords[-1][0],
-            coords[-1][1],
-            stroke=element.style.stroke_color,
-            stroke_width=element.style.line_width * element.style.stroke_width_factor,
-            class_="sheet",
-        )
+        return draw_non_element(element)
 
     # Create path
     path = Path(
