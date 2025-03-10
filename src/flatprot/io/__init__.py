9e85e5f0a55762194e4fdf222bc0ef81b977c958
Tobias O <tobias.olenyi@tum.de>
2025-03-10 18:22:22 +0100

    feat: add style  parsing

--- src/flatprot/io/__init__.py
+++ src/flatprot/io/__init__.py
@@ -4,5 +4,7 @@
 """Input/output utilities for FlatProt."""
 
 from flatprot.io.annotations import AnnotationParser
+from flatprot.io.styles import StyleParser
+from flatprot.io.structure_gemmi_adapter import GemmiStructureParser
 
-__all__ = ["AnnotationParser"]
+__all__ = ["AnnotationParser", "StyleParser", "GemmiStructureParser"]
